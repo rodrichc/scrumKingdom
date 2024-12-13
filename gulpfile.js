97d1b904236329a1f1b5d276df78c1a50e954acc
rodrichc <rodrichc1@gmail.com>
2024-12-12 23:46:03 -0300

    header casi terminado

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// import path from 'path';
+// import fs from 'fs';
+import { src, dest, watch, series } from 'gulp'
+import * as dartSass from 'sass' 
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+// import terser from 'gulp-terser'
+// import sharp from 'sharp';
+
+// export function js (done) {
+//     src('src/js/app.js')
+//         .pipe(terser())
+//         .pipe(dest('build/js'));
+//     done();
+// };
+
+export function css( done ) {
+    src ('src/scss/app.scss', {sourcemaps: true})
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( dest('build/css', {sourcemaps: '.'}) )
+
+    done();
+};
+
+
+// export async function crop(done) {
+//     const inputFolder = 'src/img/gallery/full'
+//     const outputFolder = 'src/img/gallery/thumb';
+//     const width = 250;
+//     const height = 180;
+//     if (!fs.existsSync(outputFolder)) {
+//         fs.mkdirSync(outputFolder, { recursive: true })
+//     }
+//     const images = fs.readdirSync(inputFolder).filter(file => {
+//         return /\.(jpg)$/i.test(path.extname(file));
+//     });
+//     try {
+//         images.forEach(file => {
+//             const inputFile = path.join(inputFolder, file)
+//             const outputFile = path.join(outputFolder, file)
+//             sharp(inputFile) 
+//                 .resize(width, height, {
+//                     position: 'centre'
+//                 })
+//                 .toFile(outputFile)
+//         });
+
+//         done()
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+export function dev() {  
+    watch('src/scss/**/*.scss', css);
+    // watch('src/js/**/*.js', js);
+};
+
+
+export default series(css, dev);
